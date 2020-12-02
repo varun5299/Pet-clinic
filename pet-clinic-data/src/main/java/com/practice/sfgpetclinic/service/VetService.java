d037a757ac6ae745b2d7032266323e0dcb18b4dc
varun5299 <varun.agarwal5299@gmail.com>
2020-12-02 15:45:39 +0530

    Refactored Services Interfaces.

--- pet-clinic-data/src/main/java/com/practice/sfgpetclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/com/practice/sfgpetclinic/service/VetService.java
@@ -5,11 +5,6 @@ import com.practice.sfgpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long>{
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
